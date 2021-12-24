c08ab2de2f5ef2b13e6331cb7fe95dd2b3f199f0
michal <michal.fujarewicz@gmail.com>
2021-12-24 22:45:13 +0100

    use from resource instead of from file

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -3,7 +3,7 @@ import scala.util.{Failure, Success, Try, Using}
 
 object DataLoader {
   def apply(day: Int) = {
-    Using(Source.fromFile(s"src/main/resources/day$day.txt")) {
+    Using(Source.fromResource(s"day$day.txt")) {
       source => source.getLines().toList
     }.get
   }
