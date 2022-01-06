53398b51fe467e738b034684af1af27b828db407
michal <michal.fujarewicz@gmail.com>
2022-01-06 16:11:04 +0100

    day7

---
+++ src/main/scala/Day7.scala
@@ -0,0 +1,18 @@
+object Day7 extends App {
+  val data = DataLoader(7)
+  val positions = data.head.split(',').map(_.toInt)
+
+  val targetPositions = (0 to 1000).toList
+
+  val fuelExpenses = targetPositions.map(targetPosition => {
+    positions.map(p => math.abs(p-targetPosition)).sum
+  })
+
+  val fuelExpenses2 = targetPositions.map(targetPosition => {
+    positions.map(p => math.abs(p-targetPosition)).map(x => x*(x+1)/2).sum
+  })
+
+
+  println(fuelExpenses.min)
+  println(fuelExpenses2.min)
+}
