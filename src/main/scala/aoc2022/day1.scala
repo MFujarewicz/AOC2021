f8512214f715116b40fc01fa86b57d802293c16a
mfujarewicz <michal.fujarewicz@gmail.com>
2022-12-01 17:08:06 +0100

    day 1 one completed

---
+++ src/main/scala/aoc2022/day1.scala
@@ -0,0 +1,11 @@
+package aoc2022
+
+import toolbox.DataLoader
+
+@main def day1(): Unit = {
+  val calorieSums = DataLoader(1, 2022).map(s =>  if s.isEmpty then "X" else s"V$s").reduce(_+_).split("X").map(s => s.split("V").filter(_.nonEmpty).map(_.toInt).sum)
+  println(calorieSums.max)
+
+  val top3 = calorieSums.sorted.reverse.take(3).sum
+  println(top3)
+}
