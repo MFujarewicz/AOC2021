02715145d162a2a81a460d07378d3f58264db4dd
mfujarewicz <michal.fujarewicz@gmail.com>
2022-12-03 13:54:22 +0100

    optional loading of test data

--- src/main/scala/toolbox/DataLoader.scala
+++ src/main/scala/toolbox/DataLoader.scala
@@ -4,8 +4,11 @@ import scala.io.Source
 import scala.util.Using
 
 object DataLoader {
-  def apply(day: Int, year: Int) = {
-    Using(Source.fromResource(s"$year/day$day.txt")) {
+  def apply(day: Int, year: Int, useTestData: Boolean = false): List[String] = {
+
+    val path = if useTestData then s"$year/day${day}_test.txt" else s"$year/day$day.txt"
+
+    Using(Source.fromResource(path)) {
       source => source.getLines().toList
     }.get
   }
