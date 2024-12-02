05706be0cd846673aa906f262b16485a927e975c
michal <michal.fujarewicz@gmail.com>
2024-12-02 21:12:18 +0100

    1 2024

---
+++ src/main/scala/aoc2024/day1/day1.scala
@@ -0,0 +1,22 @@
+package aoc2024.day1
+
+import toolbox.DataLoader
+
+@main def day1(): Unit = {
+  val input = DataLoader(1, 2024)
+    .map{
+      case s"$listA   $listB" => (listA.toInt, listB.toInt)
+    }
+  val listA = input.map(_._1).sorted
+  val listB = input.map(_._2).sorted
+
+  val distances = listA.zip(listB).map{case (a, b) => math.abs(a-b)}
+
+  println(distances.sum)
+
+  val part2 = listA.map(a => {
+    listB.count(_ == a) * a
+  }).sum
+
+  println(part2)
+}
