cb18275d378968bb2d32197d85e072c8d917ffd6
michal <michal.fujarewicz@gmail.com>
2024-12-03 08:53:33 +0100

    3 2024

---
+++ src/main/scala/aoc2024/day1/day3.scala
@@ -0,0 +1,33 @@
+package aoc2024.day1
+
+import toolbox.DataLoader
+
+@main def day3(): Unit = {
+  var input = DataLoader(3, 2024, useTestData = false).mkString("")
+
+  println(input)
+//  println(input.size)
+
+  var sum = 0
+  var enableMul = 1
+
+  while (input.nonEmpty){
+    val candidateLastIndex = input.indexOf(')')
+    val candidate = input.take(candidateLastIndex+1)
+//    println(candidate)
+
+    candidate match
+      case s"mul($a,$b)" if a.size <= 3 && b.size <= 3 && a.forall(_.isDigit) && b.forall(_.isDigit ) =>
+        sum += a.toInt*b.toInt*enableMul
+      case "do()" => enableMul = 1
+      case "don't()" => enableMul = 0
+      case _ => ()
+
+    input = input.drop(1)
+  }
+
+  println(sum)
+
+
+
+}
